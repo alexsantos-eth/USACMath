e646ff0a9b22fceaca8ed8c41e45207867068c95
alexsan-dev <as62971@gmail.com>
2020-01-31 01:23:06 -0600

    Virtualize

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -14,9 +14,13 @@ import Footer from "../Footer/Footer";
 // TEXTOS
 import Strings from "../../Strings/strings.json";
 
-// ICONOS
-import "../../Icons/style.css";
+// COPIAR TEXTO
+import copy from 'copy-to-clipboard';
+
+// ICONOS Y EFECTOS
 import { useRipples } from '../../Utils/hooks';
+import { FixedSizeList as List } from 'react-window';
+import "../../Icons/style.css";
 
 // BASE DE DATOS FIREBASE
 const db = firebase.database();
@@ -31,13 +35,42 @@ const defData: Idata = { course: "", link: "", title: "", text: "", type: "", up
 // MOSTRAR TEXTO AL COMPARTIR
 let showToasts = () => { };
 
+// FUNCTION PARA PREVIUSALIZAR
+let showPreview = (str: string) => { };
+
 // LIMITAR LECTURAS Y DATOS
-let dataPart: number = 4;
 let count: number = 0;
 let cData: Idata[];
-let copy: Idata[];
+let copyD: Idata[];
 
-interface State { data: [Idata[], Idata[]]; showMore?: boolean }
+interface State { data: Idata[]; showMore?: boolean; preview?: string }
+
+// ANIMAR HACIA ARRIBA
+const scrollTop = () => window.scrollTo({
+  behavior: "smooth",
+  left: 0,
+  top: 0
+});
+
+// LIMITAR SHARE API
+let shareCount: number = 0;
+
+// FUNCION DE COMPARTIR
+const shareAction = (e: any) => {
+  if (navigator.share && shareCount === 0) {
+    navigator
+      .share({
+        title: Strings.navbar.title,
+        text: Strings.files.shareText,
+        url: e.target?.getAttribute("data-link")
+      })
+      .then(() => console.log("Successfully share"))
+      .catch((error: Error) => console.log("Error sharing", error));
+  } else {
+    copy(e.target?.getAttribute("data-link"))
+    showToasts();
+  }
+}
 
 // COMPONENTE
 const App: React.FC = () => {
@@ -45,16 +78,37 @@ const App: React.FC = () => {
   useRipples();
 
   // ACTUALIZAR ESTADO CON DATOS OBTENIDOS
-  const [data, setData]: [State, Dispatch<SetStateAction<State>>] = useState({ data: [[defData], [defData]] });
+  const [data, setData]: [State, Dispatch<SetStateAction<State>>] = useState({ data: [defData] });
+
   if (count === 0) getData((data: firebase.database.DataSnapshot) => {
     const dataS: Idata[] = data.val();
     cData = dataS;
-    copy = dataS;
-    setData({
-      data: [dataS.slice(0, dataPart), dataS.slice(dataPart)]
-    })
+    copyD = dataS;
     count++;
+    setData({ data: dataS })
   })
+
+  // MOSTRAR LISTAS DE ARVHIVOS
+  const Row = ({ index, style }: { index: number, style: any }) => {
+    let c = data.data[index];
+    return (
+      <div id="filecontainer" style={style}>
+        <Files
+          showToast={showToasts}
+          shareAction={shareAction}
+          showPreview={showPreview}
+          index={index}
+          course={c.course}
+          link={c.link}
+          text={c.text}
+          title={c.title}
+          type={c.type}
+          upload={c.upload}
+        />
+      </div>
+    )
+  }
+
 
   // OBTENER TEXTO DE BUSQUEDA
   const getVal = (val: string) => {
@@ -62,39 +116,22 @@ const App: React.FC = () => {
     let searchData: Idata[] = [];
     for (let i: number = 0; i < cData.length; i++) {
       if (
-        copy[i].course.includes(res) ||
-        copy[i].link.includes(res) ||
-        copy[i].text.includes(res) ||
-        copy[i].title.includes(res) ||
-        copy[i].type.includes(res)
-      ) searchData.push(copy[i]);
+        copyD[i].course.includes(res) ||
+        copyD[i].link.includes(res) ||
+        copyD[i].text.includes(res) ||
+        copyD[i].title.includes(res) ||
+        copyD[i].type.includes(res)
+      ) searchData.push(copyD[i]);
     }
-    setData({
-      data: [searchData.slice(0, dataPart), searchData.slice(dataPart)]
-    })
+    setData({ data: searchData })
   }
-
-  // BOTON DE MOSTRAR MAS
-  const showMore = () => {
-    setData({
-      data: data.data,
-      showMore: !data.showMore
-    });
-  }
-
-  // ANIMAR HACIA ARRIBA
-  const scrollTop = () => window.scrollTo({
-    behavior: "smooth",
-    left: 0,
-    top: 0
-  });
 
   useEffect(() => {
-    //BOTON DE SUBIR
+    //BOTON DE SUBIR, VISTA PREVIA Y VIEWPORT
     const up: HTMLSpanElement | null = document.querySelector(".floating") as HTMLSpanElement;
-
-    // LIMITAR A 6 ARCHIVOS POR PAGINA EN ESCRITORIO
-    if (window.innerWidth >= 720) dataPart = 6;
+    const togglePreview: HTMLInputElement | null = document.getElementById("togglePreview") as HTMLInputElement;
+    const vp: HTMLMetaElement | null = document.getElementById("viewport") as HTMLMetaElement;
+    const closePrev: HTMLButtonElement | null = document.querySelector(".closePrev") as HTMLButtonElement;
 
     // SELECCIONAR MENSAJE DE ALERTA INFERIOR Y ESTADO DE CONEXION
     const toast: NodeListOf<HTMLDivElement> | null = document.querySelectorAll(".toast") as NodeListOf<HTMLDivElement>;
@@ -127,25 +164,60 @@ const App: React.FC = () => {
       else up.style.transform = "scale(0)";
     })
 
-  })
+    // MOSTRAR VISTA PREVIA
+    showPreview = (link: string) => {
+      setData({ data: data.data, preview: link });
+      togglePreview.checked = true;
+      vp?.setAttribute("content", "width=device-width, initial-scale=1")
+    }
 
+    // QUITAR ZOOM
+    closePrev.addEventListener("click", () => {
+      vp?.setAttribute("content", "width=device-width, initial-scale=1, user-scalable=no")
+    })
+  })
   return (
     <>
       <Navbar {...Strings.navbar} getVal={getVal} />
+      <input type="checkbox" id="togglePreview" />
+
       <div className="App">
         <div id="prels">
-          {data.data[0].length <= 1 && <Preloader />}
-          {data.data[0].length <= 1 && window.innerWidth >= 720 && <Preloader />}
+          {data.data.length <= 1 && <>
+            <Preloader />
+            <Preloader />
+            <Preloader />
+            <Preloader />
+          </>}
         </div>
-        {data.data[0].length > 1 && <Files showToast={showToasts} data={data.data[0]} appName={Strings.navbar.title} {...Strings.files} />}
-        {data.data[0].length > 1 && <button onClick={showMore} id="showMore" className={data.showMore ? "waves waves-dark active" : "waves waves-dark"}><i className="material-icons">{data.showMore ? "expand_less" : "expand_more"}</i>{data.showMore ? Strings.showLess : Strings.showMore}</button>}
-        {data.showMore && <Files showToast={showToasts} data={data.data[1]} appName={Strings.navbar.title} {...Strings.files} />}
+
+        {data.data.length > 1 && <List
+          height={window.innerHeight + 200}
+          itemCount={data.data.length}
+          itemSize={300}
+          width={window.innerWidth >= 1024 ? (window.innerWidth - window.innerWidth * 0.6) - 40 : window.innerWidth}
+        >{Row}
+        </List>
+        }
       </div>
+
       <Toast text={Strings.toast.text_2} />
       <Toast text={Strings.toast.text_1} />
       <Toast text={Strings.files.share} />
-      <Footer {...Strings.rights} />
+
       <span onClick={scrollTop} className="material-icons floating waves">arrow_upward</span>
+
+      <div className="preview">
+        <label htmlFor="togglePreview" className="material-icons closePrev waves waves-dark">close</label>
+        {data.preview &&
+          <iframe
+            width="100%"
+            height="100%"
+            title={data.preview}
+            src={`https://docs.google.com/gview?embedded=true&url=${data.preview}`}
+          />
+        }
+      </div>
     </>
   );
 }
--- src/Components/Files/Files.tsx
+++ src/Components/Files/Files.tsx
@@ -1,95 +1,59 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 // ESTILOS
 import "./Files.css";
 
-// COPIAR TEXTO
-import copy from 'copy-to-clipboard';
-
-// LIMITAR SHARE API
-let shareCount: number = 0;
-
 // PROPIEDADES
-interface Props { data: Idata[], appName: string; shareText: string; showToast: Function }
+interface Props extends Idata {
+	index: number;
+	showToast: Function;
+	shareAction: (event: any) => void;
+	showPreview: (event: any) => void
+}
 
 const File: React.FC<Props> = (props: Props) => {
-	// FUNCION DE COMPARTIR
-	const shareAction = (e: any) => {
-		if (navigator.share && shareCount === 0) {
-			navigator
-				.share({
-					title: props.appName,
-					text: props.shareText,
-					url: e.target?.getAttribute("data-link")
-				})
-				.then(() => console.log("Successfully share"))
-				.catch((error: Error) => console.log("Error sharing", error));
-		} else {
-			copy(e.target?.getAttribute("data-link"))
-			props.showToast();
-		}
+
+	const showPreview = (e: any) => {
+		const el = e.target as HTMLButtonElement;
+		const link = el?.getAttribute("data-link");
+		props.showPreview(link);
 	}
-
-	useEffect(() => {
-		// SELECCIONAR VIEWPORT Y BOTONES DE ABRIR Y CERRAR
-		const vp: HTMLMetaElement | null = document.getElementById("viewport") as HTMLMetaElement;
-		const visibilityBtn: NodeListOf<HTMLButtonElement> = document.querySelectorAll(".showPrev");
-		const closePrevBtn: NodeListOf<HTMLLabelElement> = document.querySelectorAll(".closePrev");
-
-		// HABILITAR ZOOM EN VISTA PREVIA
-		if (visibilityBtn && closePrevBtn) {
-			visibilityBtn.forEach((el: HTMLButtonElement) => {
-				el.addEventListener("click", () => vp?.setAttribute("content", "width=device-width, initial-scale=1"))
-			})
-			closePrevBtn.forEach((el: HTMLLabelElement) => {
-				el.addEventListener("click", () => vp?.setAttribute("content", "width=device-width, initial-scale=1, user-scalable=no"))
-			})
-		}
-	})
 
 	return (
 		<>
-			<div id="filecontainer">
-				{
-					props.data.map((e: Idata, i: number) => {
-						return (
-							<div key={i} className="file">
-								<input id={`showPreview${i}`} className="prevBtn" type="checkbox"></input>
-								<div className="fileHead">
-									<h1>{e.title}</h1>
-									<p>{e.text[0]?.toUpperCase() + e.text.substr(1)}</p>
-								</div>
-								<div className="fileMiddle">
-									<ul>
-										<li>
-											<i>Subido:</i>
-											<span>{e.upload}</span>
-										</li>
-										<li>
-											<i>Curso:</i>
-											<span>{e.course}</span>
-										</li>
-										<li>
-											<i>Tipo:</i>
-											<span>{e.type}</span>
-										</li>
-									</ul>
-								</div>
-								<div className="fileBody">
-									<div className="actions">
-										<button className="waves waves-dark action showPrev"><label htmlFor={`showPreview${i}`} className="material-icons">visibility</label></button>
-										<button onClick={shareAction} data-link={e.link} className="waves waves-dark action"><i data-link={e.link} className="material-icons">share</i></button>
-									</div>
-									<a className="waves waves-dark" href={e.link} title={e.title} download><i className="material-icons">arrow_downward</i> Descargar</a>
-								</div>
-								<div className="preview">
-									<label htmlFor={`showPreview${i}`} className="material-icons closePrev waves waves-dark">close</label>
-									<iframe width="100%" height="100%" title={e.title} src={`https://docs.google.com/gview?embedded=true&url=${e.link}`} />
-								</div>
-							</div>
-						)
-					})
-				}
+			<div key={props.index} className="file">
+				<div className="fileHead">
+					<h1>{props.title}</h1>
+					<p>{props.text[0]?.toUpperCase() + props.text.substr(1)}</p>
+				</div>
+				<div className="fileMiddle">
+					<ul>
+						<li>
+							<i>Subido:</i>
+							<span>{props.upload}</span>
+						</li>
+						<li>
+							<i>Curso:</i>
+							<span>{props.course}</span>
+						</li>
+						<li>
+							<i>Tipo:</i>
+							<span>{props.type}</span>
+						</li>
+					</ul>
+				</div>
+				<div className="fileBody">
+					<div className="actions">
+						<button onClick={showPreview} data-link={props.link} className="waves waves-dark action showPrev">
+							<i data-link={props.link} className="material-icons">visibility</i>
+						</button>
+						<button onClick={props.shareAction} data-link={props.link} className="waves waves-dark action">
+							<i data-link={props.link} className="material-icons">share</i>
+						</button>
+					</div>
+
+					<a className="waves waves-dark" href={props.link} title={props.title} download><i className="material-icons">arrow_downward</i> Descargar</a>
+				</div>
 			</div>
 		</>
 	)
